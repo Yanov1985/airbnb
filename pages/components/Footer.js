ebd71ec0179b1af135c1065ba8eac749175cc075
HardyGuardy <yanov.kirill@gmail.com>
2023-02-28 09:18:51 +0300

    air
    air

---
+++ pages/components/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Footer() {
+	return (
+		<div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100
+		text-gray-600 '>
+			<div className='space-x-4'>
+				<h5 className='font-bold'>LOREM</h5>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+			</div>
+
+			<div className='space-x-4'>
+				<h5 className='font-bold'>LOREM</h5>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+			</div>
+
+			<div className='space-x-4'>
+				<h5 className='font-bold'>LOREM</h5>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+			</div>
+
+			<div className='space-x-4'>
+				<h5 className='font-bold'>LOREM</h5>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+				<p>Lorem text</p>
+			</div>			
+		</div>
+
+	)
+}
+
+export default Footer
